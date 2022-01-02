5c8187e014d51af4065ac5d8726d856c8ca38855
Dharmesh Gajera <dharmeshg27@gmail.com>
2022-01-02 17:00:23 +0000

    dpg_fct_02

--- models/marts/core/fct_customer_orders_02.sql
+++ models/marts/core/fct_customer_orders_02.sql
@@ -30,13 +30,13 @@ payment_aggrigate as (
 
 customer_orders as (
     select 
-        c.customer_id
-        , min(order_date) as first_order_date
-        , max(order_date) as most_recent_order_date
+        customers.customer_id
+        , min(orders.order_date) as first_order_date
+        , max(orders.order_date) as most_recent_order_date
         , count(orders.order_id) as number_of_orders
-        from customers c 
+        from customers 
 
-        left join orders on orders.customer_id = c.customer_id 
+        left join orders on orders.customer_id = customers.customer_id 
         group by 1
 ),
 
@@ -46,43 +46,43 @@ paid_orders as (
         orders.customer_id,
         orders.order_date as order_placed_at,
         orders.status as order_status,
-        pa.total_amount_paid,
-        pa.payment_finalized_date,
-        c.first_name as customer_first_name,
-        c.last_name as customer_last_name
+        payment_aggrigate.total_amount_paid,
+        payment_aggrigate.payment_finalized_date,
+        customers.first_name as customer_first_name,
+        customers.last_name as customer_last_name
         from orders
 
-        left join payment_aggrigate pa
-                on orders.order_id = pa.order_id
-        left join customers c 
-                on orders.customer_id = c.customer_id 
+        left join payment_aggrigate
+                on orders.order_id = payment_aggrigate.order_id
+        left join customers
+                on orders.customer_id = customers.customer_id 
 ),
 
 paid_orders_self as (
 
     select
-        p.order_id,
+        t1.order_id,
         sum(t2.total_amount_paid) as clv_bad
-        from paid_orders p
-        left join paid_orders t2 on p.customer_id = t2.customer_id 
-                and p.order_id >= t2.order_id
+        from paid_orders t1
+        left join paid_orders t2 on t1.customer_id = t2.customer_id 
+                and t1.order_id >= t2.order_id
         group by 1
-        order by p.order_id
+        order by 1
 ),
 
 final as (
     select
-        p.*,
-        row_number() over (order by p.order_id) as transaction_seq,
-        row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
-        case when c.first_order_date = p.order_placed_at
+        paid_orders.*,
+        row_number() over (order by paid_orders.order_id) as transaction_seq,
+        row_number() over (partition by paid_orders.customer_id order by paid_orders.order_id) as customer_sales_seq,
+        case when customer_orders.first_order_date = paid_orders.order_placed_at
         then 'new'
         else 'return' end as nvsr,
-        x.clv_bad as customer_lifetime_value,
-        c.first_order_date as fdos
-        from paid_orders p
-        left join customer_orders as c using (customer_id)
-        left outer join paid_orders_self x on x.order_id = p.order_id
+        paid_orders_self.clv_bad as customer_lifetime_value,
+        customer_orders.first_order_date as fdos
+        from paid_orders
+        left join customer_orders on paid_orders.customer_id=customer_orders.customer_id
+        left join paid_orders_self on paid_orders_self.order_id = paid_orders.order_id
         order by order_id
 )
 
